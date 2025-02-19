97347c01a59a55652bdd6c9680984c5a2217256f
Administrator <wilhelm8@go.stockton.edu>
2025-02-19 17:37:43 -0500

    Create homepage and gallary page

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return '''
+        <h1>DenHeyer Electric</h1>
+        <a href="/gallery">Go to Gallery</a>
+    '''
+@app.route('/gallery')
+def gallery():
+    return 'Welcome to the gallery'
+    #front end please add a return link in order to return to home
 
 
 if __name__ == '__main__':
