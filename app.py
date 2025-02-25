c84aad3088fbdfbd587e0eb694e41e051d6cd374
wilhelm-SU <wilhelm8@go.stockton.edu>
2025-02-25 14:36:29 -0500

    started working on employee login and employee portal

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from urllib import request
 
 from flask import Flask, request, render_template
-import mysql.connector
+from markupsafe import escape
+#escape is extremely important, must be used on all user submitted arguments to prevent malicious actions
 
 #notes
 #some useful API
 #https://docs.python.org/3/library/http.html#module-http
-#https://flask.palletsprojects.com/en/stable/api/
+#https://flask.palletsprojects.com/en/stable/
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -39,8 +40,24 @@ def writeAReview():
         print(name, email, review)
         return 'Thanks for your review'
 
-    return render_template('htmlname.html')
+    return render_template('')
     #front end create HTML file that functions as a form and is called here
+
+@app.route('/employeeLogin', methods=['GET', 'POST'])
+def employeeLogin():
+    if request.method == 'POST':
+        'Insert username and password'
+        username = request.form['username']
+        password = request.form['password']
+        if username == '<USERNAME>' and password == '<PASSWORD>':
+            return 'not finished'
+
+    return render_template('')
+    #front end create HTML file that functions as a login page called here
+
+@app.route('/employeePortal', methods=['GET', 'POST'])
+def employeePortal():
+    return 'not finished'
 
 if __name__ == '__main__':
     app.run()
