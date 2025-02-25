bf07e5316e441a174c9d6624af6b7a8d05311dd2
Administrator <wilhelm8@go.stockton.edu>
2025-02-25 18:43:09 -0500

    Updated employee login to redirect to employee portal when logged in. Implemented session object which will only allow users who have logged in to access employeePortal. Added logout route which will be included in the employee portal

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from urllib import request
 
-from flask import Flask, request, render_template
-from markupsafe import escape
-#escape is extremely important, must be used on all user submitted arguments to prevent malicious actions
+from flask import Flask, request, render_template, session
+from markupsafe import escape #escape is extremely important, must be used on all user submitted arguments to prevent malicious actions
 
 #notes
 #some useful API
 #https://docs.python.org/3/library/http.html#module-http
 #https://flask.palletsprojects.com/en/stable/
 app = Flask(__name__)
+app.secret_key = '<KEY>' #placeholder we need to make that key hard to guess and super secret
 @app.route('/')
 def home():
     return '''
         <h1>DenHeyer Electric</h1>
-        <a href="/gallery">Go to Gallery, </a>
-        <a href="/reviews">Go to Reviews</a>
+        <a href="/gallery">Go to Gallery </a><br>
+        <a href="/reviews">Go to Reviews</a><br>
+        <a href="/employeeLogin">Employee Login</a><br>
     '''
 @app.route('/gallery')
 def gallery():
@@ -24,7 +25,7 @@ def gallery():
 @app.route('/reviews', methods=['GET'])
 def reviews():
     return '''
-        Welcome to the reviews, 
+        Welcome to the reviews<br>
         <a href="/writeAReview">Write a review!</a>
     '''
     #database of reviews would be displayed here
@@ -46,18 +47,28 @@ def writeAReview():
 @app.route('/employeeLogin', methods=['GET', 'POST'])
 def employeeLogin():
     if request.method == 'POST':
-        'Insert username and password'
         username = request.form['username']
         password = request.form['password']
         if username == '<USERNAME>' and password == '<PASSWORD>':
-            return 'not finished'
+            session['loggedIn'] = True
+            redirect(url_for('employeePortal'))
+        else:
+            return 'Invalid username or password'
 
     return render_template('')
     #front end create HTML file that functions as a login page called here
 
 @app.route('/employeePortal', methods=['GET', 'POST'])
 def employeePortal():
+    if not session.get('loggedIn'): #extremely important as this prevents non authorized users from accessing pages by simplying writing in url
+        return redirect(url_for('employeeLogin'))
+
     return 'not finished'
+
+@app.route('/logout')
+def logout():
+    session ['loggedIn'] = False
+    return redirect(url_for('employeeLogin'))
 
 if __name__ == '__main__':
     app.run()
