42877388547d22e5e7cf4444f4362760f8e4653c
wilhelm-SU <wilhelm8@go.stockton.edu>
2025-04-07 19:29:01 -0400

    Fixed and finished reset password functionality, code can easily be reeused to implement reset username functionality. Added link to employee portal.

--- routes/employee.py
+++ routes/employee.py
@@ -189,33 +189,14 @@ def galleryManager():
             print(f"Error closing connection: {e}")
 
 
-
 @employeeBP.route('/resetPassword', methods=['GET', 'POST'])
 def resetPassword():
     if not session.get('loggedIn'): #extremely important as this prevents non authorized users from accessing pages by simplying writing in url
         return redirect(url_for('employee.employeeLogin'))
 
-    reenterPassword = """
-            <form method="POST">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required><br><br>
-                <input type="submit" value="Submit">
-                <a href="/">Return</a>
-            </form>
-            """
-
-    resetPasswordHTML = """
-        <form method="POST">
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
-            <label for="password2">Password again:</label>
-            <input type="password" id="password2" name="password2" required><br><br>
-            <input type="submit" value="Submit">
-        """
-
-    if request.method == 'POST':
-        try:
-            username = session.get('username')
+    username = session.get('username')
+    if 'verified' not in session:
+        if request.method == 'POST':
             password = request.form.get('password')
 
             if not password:
@@ -223,40 +204,72 @@ def resetPassword():
 
             connect = connect_to_db()
             cursor = connect.cursor()
-            cursor.execute('SELECT * FROM "EMPLOYEE_CREDENTIALS" WHERE "EMPLOYEE_USERNAME" = %s AND "EMPLOYEE_PASSWORD" = %s'),
-            (username, password)
-            employee = cursor.fetchone()
 
-            if employee:
-                if request.method == 'POST':
-                    if request.form.get('password') != request.form.get('password2'):
-                        return ("""Passwords don't match <br>
-                                <a href="/resetPassword">Return</a>
-                                """)
-                    else:
-                        newPassword = request.form.get('password')
-                        cursor.execute('INSERT INTO "EMPLOYEE_CREDENTIALS" ("PASSWORD") VALUES (%s)', (newPassword))
-                        connect.commit()
-
-                    return'''Password successfully changed
-                    <a href="/employeePortal">Return</a>
-                    '''
-                return resetPasswordHTML
-                return resetPasswordHTML
-
-            else:
-                return ('''Invalid credentials. Please try again.<br>
-                        "<a href="/employeeLogin">Return</a><br>''', 401)
-        finally:
             try:
-                if cursor:
-                    cursor.close()
-                if connect:
-                    connect.close()
-            except Exception as e:
-                print(f"Error closing connection: {e}")
+                cursor.execute('SELECT * FROM "EMPLOYEE_CREDENTIALS" WHERE "EMPLOYEE_USERNAME" = %s AND "EMPLOYEE_PASSWORD" = %s',
+                (username, password))
+                employee = cursor.fetchone()
 
-    return reenterPassword
+                if employee:
+                    session['verified'] = True
+                    return redirect(url_for('employee.resetPassword'))
+                else:
+                    return'''Invalid credentials. Please try again.<br>
+                    <a href="/resetPassword">Return</a>'''
+
+            finally:
+                cursor.close()
+                connect.close()
+
+        return """
+        <form method="POST">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <input type="submit" value="Submit">
+            <a href="/">Return</a>
+        </form>
+        """
+
+    else:
+        if request.method == 'POST':
+            username = session.get('username')
+            password1 = request.form.get('password1')
+            password2 = request.form.get('password2')
+
+            if not password1 or not password2:
+                return '''Must enter password <br>
+                        <a href="/resetPassword">Return</a>'''
+
+            if password1 != password2:
+                return """Passwords don't match <br>
+                        <a href="/resetPassword">Return</a>
+                        """
+
+            connect = connect_to_db()
+            cursor = connect.cursor()
+            try:
+                cursor.execute('UPDATE "EMPLOYEE_CREDENTIALS" SET "EMPLOYEE_PASSWORD" = %s WHERE "EMPLOYEE_USERNAME" = %s', (password1, username))
+                connect.commit()
+
+            finally:
+                cursor.close()
+                connect.close()
+                session.pop('verified', None)
+
+            return'''Password successfully changed
+            <a href="/employeePortal">Return</a>
+            '''
+
+        return """
+        <form method="POST">
+            <label for="password1">New Password:</label>
+            <input type="password" id="password1" name="password1" required><br><br>
+            <label for="password2">Confirm New Password:</label>
+            <input type="password" id="password2" name="password2" required><br><br>
+            <input type="submit" value="Submit">
+        """
+
+
 
 @employeeBP.route('/logout')
 def logout():
